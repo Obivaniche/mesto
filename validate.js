45f57fa20e6a8a5419dfb11d37c27651856f6e68
Vladislav <obivaniche@yandex.ru>
2022-05-11 18:48:56 +0300

    Рефакторинг Validate
    Рефакторинг с использованием Rest

--- validate.js
+++ validate.js
@@ -1,60 +1,50 @@
-// Массив объектов для валидации
-const validationObj = {
-    formSelector: '.form',
-    inputSelector: '.form__input',
-    submitButtonSelector: '.form__save-button',
-    inactiveButtonClass: 'form__save-button_disabled',
-    inputErrorClass: 'form__input-error',
-    errorClass: 'form__input-error_active'
-};
-
 // Показываем ошибки
-const showInputError = (formElement, inputElement, errorMessage, validationObj) => {
+const showInputError = (formElement, inputElement, errorMessage, {inputErrorClass, errorClass}) => {
     const errorElement = formElement.querySelector(`.${inputElement.id}-error`); // Находим все ошибки
-    inputElement.classList.add(validationObj.inputErrorClass); // Показываем ошибки
+    inputElement.classList.add(inputErrorClass); // Показываем ошибки
     errorElement.textContent = errorMessage; // Показываем сообщение ошибки
-    errorElement.classList.add(validationObj.errorClass); // Отмечаем поле с ошибкой 
+    errorElement.classList.add(errorClass); // Отмечаем поле с ошибкой 
 };
 
 // Скрываем ошибки
-const hideInputError = (formElement, inputElement, validationObj) => {
+const hideInputError = (formElement, inputElement, {inputErrorClass, errorClass}) => {
     const errorElement = formElement.querySelector(`.${inputElement.id}-error`); // Находим все ошибки
-    inputElement.classList.remove(validationObj.inputErrorClass); // Скрываем ошибки
-    errorElement.classList.remove(validationObj.errorClass); // Снимаем отметку с поля 
+    inputElement.classList.remove(inputErrorClass); // Скрываем ошибки
+    errorElement.classList.remove(errorClass); // Снимаем отметку с поля 
     errorElement.textContent = ''; // Удаляем сообщение об ошибке
 };
 
 // Првоеряем валидацию и скрываем или показываем ошибки
-const checkInputValidity = (formElement, inputElement, validationObj) => {
+const checkInputValidity = (formElement, inputElement, rest) => {
     if (!inputElement.validity.valid) {
-        showInputError(formElement, inputElement, inputElement.validationMessage, validationObj); // Если валидация не прошла показываем ошибки
+        showInputError(formElement, inputElement, inputElement.validationMessage, rest); // Если валидация не прошла показываем ошибки
     } else {
-        hideInputError(formElement, inputElement, validationObj);// Если валидация прошла скрываем ошибки
+        hideInputError(formElement, inputElement, rest);// Если валидация прошла скрываем ошибки
     }
 };
 
 // Прикрепляем обработчики к полям формы и кнопке отправить
-const setEventListeners = (formElement, validationObj) => {
-    const inputList = Array.from(formElement.querySelectorAll(validationObj.inputSelector)); // Создаем коллекцию из полей ввода
-    const buttonElement = formElement.querySelector(validationObj.submitButtonSelector); // Находим кнопку сохранить
+const setEventListeners = (formElement, {inputSelector, submitButtonSelector, inactiveButtonClass, ...rest }) => {
+    const inputList = Array.from(formElement.querySelectorAll(inputSelector)); // Создаем коллекцию из полей ввода
+    const buttonElement = formElement.querySelector(submitButtonSelector); // Находим кнопку сохранить
     // Проверяем формы  
     inputList.forEach((inputElement) => {
         inputElement.addEventListener('input', function () {
-            checkInputValidity(formElement, inputElement, validationObj); // проверяем валидацию
-            toggleButtonState(inputList, buttonElement, validationObj); // Включаем/выключаем кнопку сохранить/создать
+            checkInputValidity(formElement, inputElement, rest); // проверяем валидацию
+            toggleButtonState(inputList, buttonElement, inactiveButtonClass); // Включаем/выключаем кнопку сохранить/создать
         });
     });
 };
 
 // Прикрепляем обработчики на элементы форм
-const enableValidation = (validationObj) => {
-    const formList = Array.from(document.querySelectorAll(validationObj.formSelector)); // Создаем коллекцию из элементов форм
+const enableValidation = ({formSelector, ...rest}) => {
+    const formList = Array.from(document.querySelectorAll(formSelector)); // Создаем коллекцию из элементов форм
     // Находим все кнопки submit и прикрепляем к ним обработчики 
     formList.forEach((formElement) => {
         formElement.addEventListener('submit', function (evt) {
             evt.preventDefault(); // Отменяем стандартную отправку формы
         });
-        setEventListeners(formElement, validationObj);
+        setEventListeners(formElement, rest);
     });
 };
 enableValidation(validationObj); // Включаем валидацию обьектов
@@ -67,12 +57,22 @@ function hasInvalidInput(inputList) {
 };
 
 // Включаем или выключаем кнопку сохранить/создать при вводе информации 
-function toggleButtonState(inputList, buttonElement, validationObj) {
+function toggleButtonState(inputList, buttonElement, inactiveButtonClass) {
     if (hasInvalidInput(inputList)) {
-        buttonElement.classList.add(validationObj.inactiveButtonClass); // Выключаем кнопку если поля заполнены не верно
+        buttonElement.classList.add(inactiveButtonClass); // Выключаем кнопку если поля заполнены не верно
         buttonElement.disabled = true;
     } else {
-        buttonElement.classList.remove(validationObj.inactiveButtonClass); // Включаем кнопку если поля заполнены верно
+        buttonElement.classList.remove(inactiveButtonClass); // Включаем кнопку если поля заполнены верно
         buttonElement.disabled = false;
     }
 };
+
+// Массив объектов для валидации
+enableValidation({
+    formSelector: '.form',
+    inputSelector: '.form__input',
+    submitButtonSelector: '.form__save-button',
+    inactiveButtonClass: 'form__save-button_disabled',
+    inputErrorClass: 'form__input-error',
+    errorClass: 'form__input-error_active'
+});
